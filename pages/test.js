e6a62108ed60f4755f53c4948299cea90e07b37e
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MacBook-Pro.local>
2024-03-14 20:24:44 -0400

    er

---
+++ pages/test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function test() {
+  return (
+    <div className="w-[100%] h-full flex flex-col items-center justify-center">
+      <iframe
+        class="force-mobile"
+        src="https://archive.today/newest/https://www.nytimes.com/2024/02/09/travel/icon-of-the-seas-cruise-royal-caribbean.html"
+        width="100%"
+        height="1000px"
+        scrolling="yes"
+        frameborder="0px"
+        rel="nofollow"
+      ></iframe>
+    </div>
+  );
+}
