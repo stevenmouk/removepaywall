def16ad980ff6bb154c3158e59c92395d788faed
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MacBook-Pro.local>
2024-03-14 21:05:54 -0400

    er

--- pages/test.js
+++ pages/test.js
@@ -1,17 +1,26 @@
+import Head from "next/head";
 import React from "react";
 
 export default function test() {
   return (
-    <div className="w-[100%] h-full flex flex-col items-center justify-center">
-      <iframe
-        class="force-mobile"
-        src="https://archive.today/newest/https://www.nytimes.com/2024/02/09/travel/icon-of-the-seas-cruise-royal-caribbean.html"
-        width="100%"
-        height="1000px"
-        scrolling="yes"
-        frameborder="0px"
-        rel="nofollow"
-      ></iframe>
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=1024 height=100%" />
+        {/* <meta
+          name="viewport"
+          content="width=device-width; initial-scale=.5; maximum-scale=.5; minimum-scale=.5;"
+        /> */}
+      </Head>
+      <div className="w-[100%] min-h-screen flex  items-center justify-center">
+        <iframe
+          src="https://archive.today/newest/https://www.nytimes.com/2024/02/09/travel/icon-of-the-seas-cruise-royal-caribbean.html"
+          width="100%"
+          height="1000px"
+          scrolling="yes"
+          frameborder="0px"
+          rel="nofollow"
+        ></iframe>
+      </div>
+    </>
   );
 }
