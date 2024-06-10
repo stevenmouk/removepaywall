826d941115e08aa6bcb096d7e88ff61a270d6212
Steven Moukhametzianov <stevenmoukhametzianov@Stevens-MacBook-Pro.local>
2024-06-09 21:13:58 -0400

    er

--- pages/articles.js
+++ pages/articles.js
@@ -170,7 +170,7 @@ export default function Home({ context }) {
                             className="hover:text-white focus:outline-none text-white bg-red-500
                           hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg
                           text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700
-                          dark:focus:ring-red-900 w-[30%] "
+                          dark:focus:ring-red-900 w-[30%] hover:cursor-pointer"
                           >
                             <Link
                               href="https://pure.news"
