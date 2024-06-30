866d4a6464c7bbe7e2d78c9a21264f6a610e8030
Steven Mouk <70982072+stevenmouk@users.noreply.github.com>
2024-06-29 20:16:30 -0400

    weeer

--- pages/test.js
+++ pages/test.js
@@ -4,6 +4,17 @@ import React from "react";
 
 const inter = Nunito({ subsets: ["latin"] });
 
+function handleInputChange(e) {
+  e.preventDefault();
+  // console.log(e.target.value);
+  setInput(e.target.value.split("?")[0]);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  // console.log(e.target[0].value);
+}
+
 export default function test() {
   return (
     <>
@@ -16,10 +27,65 @@ export default function test() {
 
       <div className="fixed bg-[#FFFEF0]  h-fit w-full top-0">
         <div
-          className={` text-rose-500 text-xl border-[2px] border-rose-500 px-3 py-3 rounded-full ${inter.className} w-fit`}
+          className={` text-rose-500 text-xl border-[2px] border-rose-500 px-3 py-3 rounded-full ${inter.className} w-fit mt-2`}
         >
           Archive Buttons
         </div>
+
+        <form
+          action="#"
+          onSubmit={handleSubmit}
+          className="w-full  flex items-center justify-center"
+        >
+          <div className="items-center w-[60%] mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+            <div className="relative w-full">
+              <label
+                htmlFor="email"
+                className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Enter Article URL
+              </label>
+              <div className="flex absolute inset-y-0  items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                className="block p-3 pl-10 w-full text-sm text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Enter Article URL"
+                id="email"
+                onInput={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              {/* <button
+                          type="submit"
+                          className="py-3 px-5  text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-red-500 border-red-500 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-red-500 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        >
+                          Search
+                        </button> */}
+            </div>
+            <button
+              type="submit"
+              className="py-3 px-5  ml-2 text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-red-500 border-red-500  rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-white dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
 
         <div className=" w-full flex items-center justify-center">
           <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
