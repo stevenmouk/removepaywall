56fcc6684c5919d2fee33b2a778fc1669b0844bd
Steven Mouk <70982072+stevenmouk@users.noreply.github.com>
2024-11-30 16:27:15 -0500

    er

--- pages/news/articles/russia-targeting-ukraine-grid-moves-to-cut-off-Its-nuclear-plant.js
+++ pages/news/articles/russia-targeting-ukraine-grid-moves-to-cut-off-Its-nuclear-plant.js
@@ -26,7 +26,7 @@ var pageData = {
   figure_license: "CC BY-SA 4.0",
   figure_license_url: "https://creativecommons.org/licenses/by/4.0/",
   page_image_src_rel: "/images/drone.webp",
-  img_url_full: "https://archivebuttons.com/images/death.webp",
+  img_url_full: "https://archivebuttons.com/images/drone.webp",
   figure_p:
     "A police forensic expert inspects fragments of a Shahed drone, after a Russian strike on residential neighbourhood.",
 };
