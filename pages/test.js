2fb6da8559651e106e911d33427ae8d7cea1df04
Steven Mouk <70982072+stevenmouk@users.noreply.github.com>
2025-01-12 18:29:16 -0500

    plz

--- pages/test.js
+++ pages/test.js
@@ -1,128 +1,234 @@
+import Image from "next/image";
 import { Nunito } from "next/font/google";
-import Head from "next/head";
-import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import Faq from "@/components/Faq";
+import { useRouter } from "next/navigation";
 
 const inter = Nunito({ subsets: ["latin"] });
 
-function handleInputChange(e) {
-  e.preventDefault();
-  // console.log(e.target.value);
-  setInput(e.target.value.split("?")[0]);
-}
+export default function Home() {
+  const [input, setInput] = useState(null);
 
-function handleSubmit(e) {
-  e.preventDefault();
-  // console.log(e.target[0].value);
-}
+  const router = useRouter();
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(e.target[0].value);
 
-export default function test() {
+    router.push(`/testArticle?article=${e.target[0].value}`);
+  }
+
+  function handleStuff(e) {
+    e.preventDefault();
+    if (input != null && input.trim() != "") {
+      window.open(`https://archive.is/newest/${input.trim()}`);
+    }
+  }
+
+  function handleStuff2(e) {
+    e.preventDefault();
+    if (input != null && input.trim() != "") {
+      window.open(`https://web.archive.org/${input.trim()}`);
+    }
+  }
+  function handleStuff3(e) {
+    if (input != null && input.trim() != "") {
+      e.preventDefault();
+      window.open(`https://webcache.googleusercontent.com/search?q=cache:${input.trim()}`);
+    }
+  }
+  function handleStuff4(e) {
+    e.preventDefault();
+    if (input != null && input.trim() != "") {
+      window.open(`https://removepaywall.com/${input.trim()}`);
+    }
+  }
+
+  function handleInputChange(e) {
+    e.preventDefault();
+    // console.log(e.target.value);
+    setInput(e.target.value.split("?")[0]);
+  }
+  function handleStuff5(e) {
+    e.preventDefault();
+    if (input != null && input.trim() != "") {
+      window.open(`https://12ft.io/${input.trim()}`);
+    }
+  }
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width; initial-scale=.5; maximum-scale=.5; minimum-scale=.5;"
-        />
-      </Head>
-
-      <div className="fixed bg-[#FFFEF0]  h-fit w-full top-0">
-        <div
-          className={` text-rose-500 text-xl border-[2px] border-rose-500 px-3 py-3 rounded-full ${inter.className} w-fit mt-2`}
-        >
-          Archive Buttons
-        </div>
-
-        <form
-          action="#"
-          onSubmit={handleSubmit}
-          className="w-full  flex items-center justify-center"
-        >
-          <div className="items-center w-[60%] mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
-            <div className="relative w-full">
-              <label
-                htmlFor="email"
-                className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+      <main>
+        <nav className="bg-[#FFFEF0] border-gray-200 px-4 lg:px-6 pt-2.5 ">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+            <Link href="/" className="cursor-pointer">
+              <div
+                className={` text-rose-500 text-xl border-[2px] border-rose-500 px-3 py-3 rounded-full ${inter.className}`}
               >
-                Enter Article URL
-              </label>
-              <div className="flex absolute inset-y-0  items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
+                Archive Buttons
               </div>
-              <input
-                className="block p-3 pl-10 w-full text-sm text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Enter Article URL"
-                id="email"
-                onInput={handleInputChange}
-                required
-              />
-            </div>
-            <div>
-              {/* <button
+            </Link>
+            <div className="flex items-center lg:order-2"></div>
+            <div
+              className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+              id="mobile-menu-2"
+            ></div>
+          </div>
+        </nav>
+
+        <section className="bg-[#FFFEF0] dark:bg-gray-900 min-h-screen pt-20">
+          <div className="flex flex-col items-center justify-between  py-8 px-4 mx-auto  lg:py-16 ">
+            <div className="place-self-center text-center lg:col-span-7">
+              <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div className="mx-auto max-w-screen-md sm:text-center">
+                  <h1 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">
+                    All of the paywall removers in one place
+                  </h1>
+                  <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">
+                    Simply enter the URL of the article and click the archive buttons to remove any
+                    paywall.
+                  </p>
+                  <form action="#" onSubmit={handleSubmit}>
+                    <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+                      <div className="relative w-full">
+                        <label
+                          htmlFor="email"
+                          className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                          Enter Article URL
+                        </label>
+                        <div className="flex absolute inset-y-0  items-center pl-3 pointer-events-none">
+                          <svg
+                            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 20 20"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                            />
+                          </svg>
+                        </div>
+                        <input
+                          className="block p-3 pl-10 w-full text-sm text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                          placeholder="Enter Article URL"
+                          id="email"
+                          onInput={handleInputChange}
+                          required
+                        />
+                      </div>
+                      <div>
+                        {/* <button
                           type="submit"
                           className="py-3 px-5  text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-red-500 border-red-500 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-red-500 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                         >
                           Search
                         </button> */}
-            </div>
-            <button
-              type="submit"
-              className="py-3 px-5  ml-2 text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-red-500 border-red-500  rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-white dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            >
-              Search
-            </button>
-          </div>
-        </form>
+                      </div>
+                      <button
+                        type="submit"
+                        className="py-3 px-5  ml-2 text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-red-500 border-red-500  rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-white dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                      >
+                        Search
+                      </button>
+                    </div>
+                    <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">
+                      We all hate paywalls, these paywall removers can help with that.
+                      {/* <a
+                        href="#"
+                        className="font-medium text-primary-600 dark:text-primary-500 hover:underline"
+                      >
+                        Read our Privacy Policy
+                      </a> */}
+                    </div>
+                    {/* <section className="bg-gray-900 rounded-xl flex flex-row mt-10">
+                      <Image
+                        src="/images/test4.png"
+                        width={200}
+                        height={100}
+                        className="object-fit w-[250px] h-[250px] rounded-xl p-3 hidden sm:inline"
+                      ></Image>
+                      <div>
+                        <div className=" px-4 mx-auto max-w-screen-xl lg:px-6">
+                          <div className="mx-auto max-w-screen-md sm:text-center">
+                            <h2 className="mb-4 text-3xl tracking-tight font-extrabold  sm:text-4xl text-white">
+                              Sign up for our Finance Newsletter
+                            </h2>
+                            <p className="mx-auto  max-w-2xl font-light   sm:text-xl text-gray-400">
+                              Stay up to date with the latest finance news and market peformance
+                              without the clutter.
+                            </p>
+                          </div>
+                        </div>
 
-        <div className=" w-full flex items-center justify-center">
-          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Archive.is
+                        <div className="w-full flex items-center justify-center">
+                          <Link
+                            href="https://pure.news"
+                            target="_blank"
+                            className=" hover:text-white focus:outline-none text-white bg-red-500
+                            hover:bg-red-700 focus:ring-4 focus:ring-red-300 font-medium rounded-lg
+                            text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700
+                            dark:focus:ring-red-900 w-[30%] hover:cursor-pointer "
+                          >
+                            Subscribe
+                          </Link>
+                        </div>
+                      </div>
+                    </section>
+                  */}
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* <Toaster /> */}
+
+        {/* <section className="bg-[#FFFEF0] dark:bg-gray-900 ">
+          <div className=" px-4 mx-auto max-w-screen-xl ">
+            <h2 className="mb-8 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 lg:mb-16 dark:text-white md:text-4xl">
+              Past Editions
+            </h2>
+          </div>
+        </section> */}
+        {/* <ArticlesGrid articles={articles} /> */}
+
+        <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700  " />
+
+        <Faq />
+
+        <footer className="p-4  bg-[#FFFEF0] sm:p-6 dark:bg-gray-800">
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+              © 2024{" "}
+              <a href="/" className="hover:underline">
+                Archive Buttons™
+              </a>
+              . All Rights Reserved.
             </span>
-          </button>
-          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Archive.org
-            </span>
-          </button>
-          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              12ft.io
-            </span>
-          </button>
-          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Removepaywall.com
-            </span>
-          </button>
-          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Google Cache
-            </span>
-          </button>
-        </div>
-      </div>
-      <div className="w-[100%] h-[100vh] overflow-scroll flex  items-center justify-center">
-        <iframe
-          src="https://archive.today/newest/https://www.nytimes.com/2024/02/09/travel/icon-of-the-seas-cruise-royal-caribbean.html"
-          width="100%"
-          height="2000px"
-          rel="nofollow"
-        ></iframe>
-      </div>
+
+            <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+              <Link className="text-gray-500 dark:hover:text-white" href="/privacy">
+                Privacy Policy
+              </Link>
+              <span>|</span>
+              <Link className="text-gray-500 dark:hover:text-white" href="/blog">
+                blog
+              </Link>
+              <span>|</span>
+              <p className="text-gray-500 dark:hover:text-white mr-24">
+                contact: archivebuttons@gmail.com
+              </p>
+            </div>
+          </div>
+        </footer>
+      </main>
     </>
   );
 }
---
+++ pages/testArticle.js
@@ -0,0 +1,275 @@
+import Image from "next/image";
+import { Nunito } from "next/font/google";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Faq from "@/components/Faq";
+import Head from "next/head";
+import { useRouter } from "next/navigation";
+
+const inter = Nunito({ subsets: ["latin"] });
+
+export default function Home({ context }) {
+  const [input, setInput] = useState("");
+  const [article, setArticle] = useState(null);
+
+  const router = useRouter();
+  useEffect(() => {
+    // const newInput = context.id.reduce((acc, part, index) => {
+    //   // Add double slash after the first part, otherwise add a single slash
+    //   const separator = index === 0 ? "" : index === 1 ? "//" : "/";
+
+    //   return acc + separator + part;
+    // }, "");
+    setInput(context.article);
+    setArticle(`https://archive.is/newest/${context.article?.trim()?.split("?")[0]}`);
+  }, [context]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(e.target[0].value);
+    router.push(`/articles?article=${e.target[0].value}`);
+  }
+
+  function handleInputChange(e) {
+    e.preventDefault();
+    // console.log(e.target.value);
+    setInput(e.target.value.split("?")[0]);
+  }
+
+  useEffect(() => {
+    function resizeIframe() {
+      const container = document.getElementById("iframe-container");
+      const iframe = document.getElementById("content-frame");
+      const containerTop = container.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      const newHeight = windowHeight - containerTop - 20;
+      // 20px for some bottom margin
+      container.style.height = `${newHeight}px`;
+
+      // Add mobile scaling
+      if (window.innerWidth < 640) {
+        // extra small devices
+        iframe.style.transform = "scale(0.5)";
+        iframe.style.width = "200%";
+        iframe.style.height = "200%";
+      } else if (window.innerWidth < 768) {
+        // small devices
+        iframe.style.transform = "scale(0.6)";
+        iframe.style.width = "166.67%";
+        iframe.style.height = "166.67%";
+      } else if (window.innerWidth < 1024) {
+        // medium devices
+        iframe.style.transform = "scale(0.8)";
+        iframe.style.width = "125%";
+        iframe.style.height = "125%";
+      } else {
+        // large devices
+        iframe.style.transform = "scale(0.99)";
+        iframe.style.width = "100%";
+        iframe.style.height = "100%";
+      }
+    }
+
+    resizeIframe();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", resizeIframe);
+  }, []);
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=.5; maximum-scale=.5; minimum-scale=.5;"
+        />
+      </Head>
+
+      <div className="absolute z-20 bg-[#FFFEF0]  h-fit w-full top-0">
+        <Link href="/" className="cursor-pointer">
+          <div
+            className={` text-rose-500 text-xl border-[2px] border-rose-500 px-3 py-3 rounded-full ${inter.className} w-fit mt-2`}
+          >
+            Archive Buttons
+          </div>
+        </Link>
+
+        <form
+          action="#"
+          onSubmit={handleSubmit}
+          className="w-full  flex items-center justify-center"
+        >
+          <div className="items-center sm:w-[60%] w-full mb-3  max-w-screen-sm flex justify-center ssm:space-y-0">
+            <div className="relative w-full">
+              <label
+                htmlFor="email"
+                className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Enter Article URL
+              </label>
+              <div className="flex absolute inset-y-0  items-center pl-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                className="block p-3 pl-10 w-full text-sm text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Enter Article URL"
+                id="email"
+                value={input}
+                onInput={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              {/* <button
+                          type="submit"
+                          className="py-3 px-5  text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-red-500 border-red-500 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-red-500 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                        >
+                          Search
+                        </button> */}
+            </div>
+            <button
+              type="submit"
+              className="h-10 w-24 ml-2 text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-red-500 border-red-500  rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-white dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+
+        <div className=" w-full flex items-center justify-center">
+          <button
+            onClick={() =>
+              setArticle(`https://archive.is/newest/${context.article?.trim()?.split("?")[0]}`)
+            }
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white"
+          >
+            <span
+              class={`relative px-5 py-2.5 transition-all ease-in duration-75  dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 hover:text-white ${
+                article?.includes("archive.is") ? "bg-red-500 text-white" : "bg-white text-black"
+              }`}
+            >
+              1
+            </span>
+          </button>
+          <button
+            onClick={() =>
+              setArticle(`https://web.archive.org/${context.article?.trim()?.split("?")[0]}`)
+            }
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white"
+          >
+            <span
+              class={`relative px-5 py-2.5 transition-all ease-in duration-75  dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 hover:text-white ${
+                article?.includes("archive.org") ? "bg-red-500 text-white" : "bg-white text-black"
+              }`}
+            >
+              2
+            </span>
+          </button>
+          <button
+            onClick={() => window.open(`https://12ft.io/${context.article?.trim()?.split("?")[0]}`)}
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white"
+          >
+            <span
+              class={`relative px-5 py-2.5 transition-all ease-in duration-75  dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 hover:text-white ${
+                article?.includes("12ft.io") ? "bg-red-500 text-white" : "bg-white text-black"
+              }`}
+            >
+              3
+            </span>
+          </button>
+          <button
+            onClick={() =>
+              window.open(`https://removepaywall.com/${context.article?.trim()?.split("?")[0]}`)
+            }
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white"
+          >
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              4
+            </span>
+          </button>
+          <button
+            onClick={() =>
+              window.open(
+                `https://webcache.googleusercontent.com/search?q=cache:${
+                  context.article?.trim()?.split("?")[0]
+                }`
+              )
+            }
+            class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group   bg-red-500  hover:text-white dark:text-white"
+          >
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              5
+            </span>
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col min-h-screen ">
+        <div id="iframe-container" className="relative flex-grow w-full">
+          <iframe
+            id="content-frame"
+            className="absolute inset-0 w-full h-full border-0"
+            style={{ transform: "scale(0.99)", transformOrigin: "0 0" }}
+            src={article}
+          />
+        </div>
+      </div>
+      {/* <hr className=" border-gray-200 sm:mx-auto dark:border-gray-700  " />
+
+      <Faq />
+
+      <footer className="p-4  bg-[#FFFEF0] sm:p-6 dark:bg-gray-800">
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            <a href="/" className="hover:underline">
+              Archive Buttons™
+            </a>
+            . All Rights Reserved.
+          </span>
+
+          <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+            <Link className="text-gray-500 dark:hover:text-white" href="/privacy">
+              Privacy Policy
+            </Link>
+            <span>|</span>
+            <Link className="text-gray-500 dark:hover:text-white" href="/blog">
+              blog
+            </Link>
+            <span>|</span>
+            <p className="text-gray-500 dark:hover:text-white mr-24">
+              contact: archivebuttons@gmail.com
+            </p>
+          </div>
+        </div>
+      </footer> */}
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+  // console.log(context);
+  // Fetch the articles from the API route
+  // console.log(context);
+  const query = context.query;
+
+  //console.log(res);
+  return {
+    props: {
+      context: query,
+    },
+  };
+}
