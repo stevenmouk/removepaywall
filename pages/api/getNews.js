d0e2d2f34140cf26fd66de4454d94924863063b3
Steven Mouk <70982072+stevenmouk@users.noreply.github.com>
2025-04-05 16:08:11 -0400

    weee

--- pages/api/getNews.js
+++ pages/api/getNews.js
@@ -37,7 +37,8 @@ export default async function handler(req, res) {
     });
 
     if (!response.ok) {
-      res.status(200).send(defaultHTML);
+      console.log(response);
+      res.status(500).send(defaultHTML);
       return;
     }
 
